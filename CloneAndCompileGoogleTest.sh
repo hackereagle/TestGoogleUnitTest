32a98e6d8dcdbed14740750916c5e96fb4cd3e8f
sclin <read0651@hotmail.com>
2023-02-08 22:16:05 +0800

    New: Created shell script to download and compile google test. Chg: Modified README.md file to record references. New: Added gitignore to ignore google test project folder and binary directory.

---
+++ CloneAndCompileGoogleTest.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git clone https://github.com/google/googletest.git
+
+# compiling google test
+cd googletest
+cmake -E make_directory build
+cmake -E chdir build/ cmake -DCMAKE_CXX_FLAGS=-std=c++11 -Dgtest_build_samples=ON ..
+cmake --build build/ --parallel 4
+
+# install
+cd build/
+make install
